6a81c4e8df8eb7ad242cf0e39deae8e6be3098e5
nacean <nacean@naver.com>
2022-07-08 19:18:02 +0900

    :hammer: eslint, prettier 세팅

---
+++ client/.prettierrc.js
@@ -0,0 +1,14 @@
+//템플릿 출처 : https://wiki.jjagu.com/?p=479
+
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  eslintIntegration: true,
+  endOfLine: 'auto',
+  parser: 'typescript',
+  useTabs: false,
+  arrowParens: 'always',
+};
