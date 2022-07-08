0a457bc7595a7be66a7e74d3dcc1d929e767b18c
nacean <nacean@naver.com>
2022-07-08 19:31:44 +0900

    :hammer: eslint와 prettier 변경, 불필요 파일들 삭제

--- client/.prettierrc.js
+++ client/.prettierrc.js
@@ -1,14 +1,13 @@
-//템플릿 출처 : https://wiki.jjagu.com/?p=479
+//템플릿 출처 : https://velog.io/@luna_runa/Next.js-TypeScript-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-ESLintAirbnb-Prettier-%EC%84%A4%EC%A0%95
 
 module.exports = {
-  trailingComma: 'es5',
-  tabWidth: 2,
-  semi: true,
-  singleQuote: true,
-  printWidth: 100,
-  eslintIntegration: true,
+  printWidth: 80, //코드 한줄 최대치
+  semi: true, //코드 마지막에 세미콜론
+  singleQuote: false, //문자열을 작은따옴표로 작성할것인지(false = 큰 따옴표)
+  trailingComma: 'all', //객체나 배열 등에 맨 마지막에도 콤마
+  tabWidth: 2, //들여쓰기 2칸(스페이스 2칸)
+  bracketSpacing: true, //객체 리터럴에서 괄호에 공백 삽입 여부
   endOfLine: 'auto',
-  parser: 'typescript',
-  useTabs: false,
-  arrowParens: 'always',
+  useTabs: false, //탭 대신 스페이스
+  arrowParens: 'avoid', // 화살표 함수에서 매개변수를 하나만 받을때 괄호 생략
 };
