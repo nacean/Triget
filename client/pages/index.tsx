f84a5fda9d0ba60b8f038f57c0bc3cc549cc50c2
nacean <nacean@naver.com>
2022-07-10 18:08:11 +0900

    :sparkles: 어떤 planning 을 하는지 물어보는 h1, planning Container 구현
    어떤 여행을~ 부분 <h1> 으로 구현, 여행 planning 및 필터링으로 위한 container 구현

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import PlanningPlace from "components/layout/mainPageComponent/planner/PlanningPlace";
+import styled from "styled-components";
 
-function home() {
-  return <div>home</div>;
+const MainAsk = styled.h1`
+  width: 100%;
+  padding: 40px 0;
+  font-size: 24px;
+  font-weight: 500;
+`;
+
+function MainPage() {
+  return (
+    <article>
+      <MainAsk>어떤 여행을 하고 싶으신가요?</MainAsk>
+      <PlanningPlace />
+    </article>
+  );
 }
 
-export default home;
+export default MainPage;
