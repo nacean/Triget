afc5f46709e3ae4aca33d6972458aa5c1c876836
nacean <nacean@naver.com>
2022-07-14 15:44:56 +0900

    :art: planningBtn component 제거 후 planningBox 안으로 이동

--- client/components/mainPageComponent/planner/PlanningBox.tsx
+++ client/components/mainPageComponent/planner/PlanningBox.tsx
@@ -1,8 +1,26 @@
+import { Slide, Button } from "@mui/material";
+import { useState } from "react";
 import styled from "styled-components";
-import BudgetWeightForm from "./budgetWeight/BudgetWeightForm";
+import { useRecoilValue } from "recoil";
+import airportState from "atoms/plannerAtoms/airportState";
+import travelKeywordState from "atoms/plannerAtoms/travelKeywordState";
+import countriesState from "atoms/plannerAtoms/countriesState";
+import startDateState from "atoms/plannerAtoms/startDateState";
+import endDateState from "atoms/plannerAtoms/endDateState";
+import peopleCntState from "atoms/plannerAtoms/peopleCntState";
+import budgetState from "atoms/plannerAtoms/budgetState";
+import activityWeightState from "atoms/plannerAtoms/weightAtoms/activityWeightState";
+import airportWeightState from "atoms/plannerAtoms/weightAtoms/airportWeightState";
+import hotelWeightState from "atoms/plannerAtoms/weightAtoms/hotelWeightState";
+import foodWeightState from "atoms/plannerAtoms/weightAtoms/foodWeightState";
 import KeyWordAndCountriesBox from "./keyWordAndCountries/KeyWordAndCountriesBox";
-import PlanningBtn from "./planningBtn/PlanningBtn";
+import BudgetWeightForm from "./budgetWeight/BudgetWeightForm";
 import RestBox from "./restForms/RestBox";
+import SearchIcon from "@mui/icons-material/Search";
+
+const PlanningSlideContainer = styled.div`
+  width: 100%;
+`;
 
 const PlanningContainer = styled.section`
   position: relative;
@@ -18,13 +36,58 @@ const PlanningContainer = styled.section`
 `;
 
 function PlanningBox() {
+  //slide state
+  const [slideMove, setSlideMove] = useState(false);
+
+  const travelKeywordValue = useRecoilValue(travelKeywordState);
+  const countryValue = useRecoilValue(countriesState);
+  const startDateValue = useRecoilValue(startDateState);
+  const endDateValue = useRecoilValue(endDateState);
+  const peopleCntValue = useRecoilValue(peopleCntState);
+  const budgetValue = useRecoilValue(budgetState);
+  const airportValue = useRecoilValue(airportState);
+
+  // recoil weight values
+  const airportWeightValue = useRecoilValue(airportWeightState);
+  const hotelWeightValue = useRecoilValue(hotelWeightState);
+  const foodWeightValue = useRecoilValue(foodWeightState);
+  const activityWeightValue = useRecoilValue(activityWeightState);
+
+  const onBtnClick = () => {
+    setSlideMove(!slideMove);
+  };
+
   return (
-    <PlanningContainer>
-      <KeyWordAndCountriesBox />
-      <RestBox />
-      <BudgetWeightForm />
-      <PlanningBtn />
-    </PlanningContainer>
+    <PlanningSlideContainer>
+      <Slide direction="right" in={!slideMove} mountOnEnter unmountOnExit>
+        <PlanningContainer>
+          <KeyWordAndCountriesBox />
+          <RestBox />
+          <BudgetWeightForm />
+          <Button
+            variant="contained"
+            endIcon={<SearchIcon />}
+            size="large"
+            sx={{
+              position: "absolute",
+              bottom: 20,
+              right: 20,
+              backgroundColor: "#000000b7",
+              borderRadius: "12px",
+              ":hover": {
+                backgroundColor: "#0000009f",
+              },
+            }}
+            onClick={onBtnClick}
+          >
+            일정표 만들기
+          </Button>
+        </PlanningContainer>
+      </Slide>
+      <Slide direction="left" in={slideMove} mountOnEnter unmountOnExit>
+        <PlanningContainer />
+      </Slide>
+    </PlanningSlideContainer>
   );
 }
 
