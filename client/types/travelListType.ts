7337b7fa7bb660ffa016d67295e6c27f3008c276
nacean <nacean@naver.com>
2022-07-23 22:11:45 +0900

    :sparkles: 일정표 생성 이후 travelList를 받기 위한 module 생성 및 type 생성
    현재는 test를 위해 임시로 관리 이후 수정 필요(related to : #24)

---
+++ client/types/travelListType.ts
@@ -0,0 +1,18 @@
+export interface travelListType {
+  travelTime: string;
+  product_id: number;
+  product_name: string;
+  thumbnail_url: string;
+  product_type: string;
+  city: string;
+  district: string;
+  address: string;
+  longitude: number;
+  latitude: number;
+  price: number;
+  currency_code: string;
+  review_score: number;
+  popularity: number;
+  product_detail_url: string;
+  keywords_array: string[];
+}
