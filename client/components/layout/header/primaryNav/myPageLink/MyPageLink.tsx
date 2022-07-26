67e16829c65bf5c5088e8a58b8139a558986db79
nacean <nacean@naver.com>
2022-07-26 20:16:33 +0900

    :lipstick: antd/icon 삭제에 따른 mypage icon 을 mui icon 으로 변경

--- client/components/layout/header/primaryNav/myPageLink/MyPageLink.tsx
+++ client/components/layout/header/primaryNav/myPageLink/MyPageLink.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { UserOutlined } from "@ant-design/icons";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import styled from "styled-components";
 
 const MyPageAnchor = styled.a`
@@ -13,15 +13,22 @@ const MyPageWord = styled.div`
   margin-right: 10px;
 `;
 
+const StyledMyPageLogo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 // Navigation 에서 myPage로 가는 버튼
 function MyPageLink() {
   return (
     <Link className="myPageLink" href="/" passHref>
       <MyPageAnchor>
         <MyPageWord>마이페이지</MyPageWord>
-        <div className="myPageLogo">
-          <UserOutlined style={{ fontSize: "24px", color: "#6F767D" }} />
-        </div>
+        <StyledMyPageLogo>
+          <PersonOutlineOutlinedIcon
+            sx={{ fontSize: "30px", color: "#6F767D" }}
+          />
+        </StyledMyPageLogo>
       </MyPageAnchor>
     </Link>
   );
