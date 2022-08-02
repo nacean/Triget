565bda9227694120a196f010bc03da6d5cbd56eb
nacean <nacean@naver.com>
2022-08-02 17:38:19 +0900

    :wrench: skyscanner 이미지 임시 활성화를 위한 next.config 수정
    related to : #33

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ["image.fmkorea.com"],
+    domains: ["image.fmkorea.com", "www.skyscanner.net"],
   },
 };
 
