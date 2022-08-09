f646a3f9e6f54e48cd5c3402593c1f7a7c906531
nacean <nacean@naver.com>
2022-08-09 20:36:22 +0900

    :sparkles: 이용약관 클릭 시 modal 을 통해 세부적인 내용 확인 가능하게 구현
    related to : #45

--- client/components/layout/footer/footerIntroduce/footerInfo/TermsOfService.tsx
+++ client/components/layout/footer/footerIntroduce/footerInfo/TermsOfService.tsx
@@ -1,12 +1,71 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import getTermsArray from "modules/termsOfServiceModule/getTermsArray";
+import { useState } from "react";
 import styled from "styled-components";
+import termsArrayType from "types/termArray/termArrayType";
 
 const StyledTermsOfService = styled.div`
   font-size: 20px;
   font-weight: 400;
+  cursor: pointer;
+`;
+
+const StyledTermTitle = styled.h2`
+  font-size: 20px;
+  color: black;
+`;
+
+const StyledTermDescription = styled.p`
+  margin-top: 10px;
 `;
 
 function TermsOfService() {
-  return <StyledTermsOfService>이용약관</StyledTermsOfService>;
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  const onModalOpen = () => {
+    setOpenModal(true);
+  };
+  const onModalClose = () => {
+    setOpenModal(false);
+  };
+
+  const termArray: termsArrayType[] = getTermsArray();
+
+  return (
+    <>
+      <StyledTermsOfService onClick={onModalOpen}>
+        이용약관
+      </StyledTermsOfService>
+      <Dialog
+        open={openModal}
+        onClose={onModalClose}
+        aria-labelledby="termsOfService-dialog-title"
+        aria-describedby="termsOfService-dialog-description"
+      >
+        <DialogTitle id="termsOfService-dialog-title">이용약관</DialogTitle>
+        <DialogContent>
+          {termArray.map(({ termTitle, termDescription }: termsArrayType) => (
+            <DialogContentText sx={{ marginBottom: 3 }}>
+              <StyledTermTitle>{termTitle}</StyledTermTitle>
+              <StyledTermDescription>{termDescription}</StyledTermDescription>
+            </DialogContentText>
+          ))}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onModalClose} color="inherit">
+            닫기
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
 
 export default TermsOfService;
