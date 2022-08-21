501e7cd3652da8372a9a9a65a42bf7d39a27d0dd
nacean <nacean@naver.com>
2022-08-21 21:44:55 +0900

    :sparkles: 공항 경유 시 modal 에서 그 사이 걸리는 시간 나오게끔 구현

--- client/components/mainPageComponent/planner/pickTravelProducts/flightComponents/flightModal/FlightModalContent.tsx
+++ client/components/mainPageComponent/planner/pickTravelProducts/flightComponents/flightModal/FlightModalContent.tsx
@@ -11,19 +11,58 @@ import flightAirportInfoType from "types/flightTypes/flightAirportInfoType";
 
 interface FlightModalContentType {
   startTime: string;
-  movingTime?: string;
   airportInfo: flightAirportInfoType;
+  movingTime?: string;
+  waitingTime?: string;
 }
 
 const StyledExactTime = styled.span`
   display: inline-block;
   width: 4.5vw;
+  height: 20px;
+`;
+
+const StyledMovingTime = styled.div`
+  width: 6vw;
+  position: absolute;
+  top: 20%;
+  right: 15px;
+  font-size: 18px;
+  font-weight: 500;
+  text-align: end;
+`;
+
+const StyledWaitingTime = styled.div`
+  width: 6vw;
+  position: absolute;
+  top: 20%;
+  left: 22px;
+  font-size: 18px;
+  font-weight: 500;
+  text-align: start;
+`;
+
+const StyledWaitingComment = styled.p`
+  width: 6vw;
+  height: 30px;
+  position: absolute;
+  top: 8%;
+  right: 15px;
+  font-size: 16px;
+  font-weight: 400;
+  background-color: #d1435b48;
+  color: #212121;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function FlightModalContent({
   startTime,
-  movingTime,
   airportInfo,
+  movingTime,
+  waitingTime,
 }: FlightModalContentType) {
   return (
     <TimelineItem>
@@ -31,16 +70,15 @@ function FlightModalContent({
         <TimelineDot />
         {movingTime && (
           <TimelineConnector sx={{ position: "relative" }}>
-            <div
-              style={{
-                width: "6vw",
-                position: "absolute",
-                top: 6,
-                right: 25,
-              }}
-            >
-              {movingTime}
-            </div>
+            <StyledMovingTime>{movingTime}</StyledMovingTime>
+          </TimelineConnector>
+        )}
+        {waitingTime && (
+          <TimelineConnector
+            sx={{ position: "relative", bgcolor: "#d1435b", color: "#d1435b" }}
+          >
+            <StyledWaitingTime>{waitingTime}</StyledWaitingTime>
+            <StyledWaitingComment>공항 경유 대기</StyledWaitingComment>
           </TimelineConnector>
         )}
       </TimelineSeparator>
--- client/components/mainPageComponent/planner/pickTravelProducts/flightComponents/flightModal/FlightModalTimeLine.tsx
+++ client/components/mainPageComponent/planner/pickTravelProducts/flightComponents/flightModal/FlightModalTimeLine.tsx
@@ -1,5 +1,6 @@
 import { Timeline } from "@mui/lab";
 import getMovingTime from "modules/timeModule/getMovingTime";
+import getWaitingTime from "modules/timeModule/getWaitingTime";
 import styled from "styled-components";
 import flightLegType from "types/flightTypes/flightLegType";
 import FlightModalContent from "./FlightModalContent";
@@ -13,24 +14,60 @@ const StyledTimeLineContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin-top: 20px;
 `;
 
 function FlightModalTimeLine({ flightLeg }: FlightModalTimeLineType) {
-  return (
-    <StyledTimeLineContainer>
-      {flightLeg.segments.map((legParam: flightLegType) => (
-        <Timeline sx={{ width: "70%" }}>
+  const getTimeLineComponent = () => {
+    const timeArray = [];
+
+    for (
+      let segIndex = 0;
+      segIndex < flightLeg.segments.length;
+      segIndex += 1
+    ) {
+      const legParam = flightLeg.segments[segIndex];
+
+      if (segIndex === flightLeg.segments.length - 1) {
+        timeArray.push(
           <FlightModalContent
             startTime={legParam.departure}
             airportInfo={legParam.origin}
             movingTime={getMovingTime(legParam.durationInMinutes)}
-          />
+          />,
+        );
+        timeArray.push(
           <FlightModalContent
             startTime={legParam.arrival}
             airportInfo={legParam.destination}
-          />
-        </Timeline>
-      ))}
+          />,
+        );
+      } else {
+        timeArray.push(
+          <FlightModalContent
+            startTime={legParam.departure}
+            airportInfo={legParam.origin}
+            movingTime={getMovingTime(legParam.durationInMinutes)}
+          />,
+        );
+        timeArray.push(
+          <FlightModalContent
+            startTime={legParam.arrival}
+            airportInfo={legParam.destination}
+            waitingTime={getWaitingTime(
+              legParam.arrival,
+              flightLeg.segments[segIndex + 1].departure,
+            )}
+          />,
+        );
+      }
+    }
+    return timeArray;
+  };
+
+  return (
+    <StyledTimeLineContainer>
+      <Timeline sx={{ width: "80%" }}>{getTimeLineComponent()}</Timeline>
     </StyledTimeLineContainer>
   );
 }
