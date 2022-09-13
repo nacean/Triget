d38af88c8c2660015b4a9b6bebb63da6677f0ea0
nacean <nacean@naver.com>
2022-09-13 23:02:57 +0900

    :construction: 로그인 버튼 클릭 시 정보 저장 및 마이페이지 나타나게 끔 기초 구현

--- client/components/loginPageComponent/container/LoginPageContainer.tsx
+++ client/components/loginPageComponent/container/LoginPageContainer.tsx
@@ -1,10 +1,15 @@
 import { Button, Divider, TextField } from "@mui/material";
+import userState from "atoms/loginAtoms/userState";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { ChangeEvent, useState } from "react";
+import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
+import userType from "types/userTypes/userType";
 import LoginTitle from "../title/LoginTitle";
 
 const StyledLoginPageContainer = styled.div`
-  width: 40%;
+  width: 30%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -18,6 +23,33 @@ const StyledSignUpLink = styled.a`
 `;
 
 function LoginPageContainer() {
+  // router
+  const router = useRouter();
+
+  // local userID, PW
+  const [userID, setUserID] = useState<string>("");
+  const [userPW, setUserPW] = useState<string>("");
+
+  // global userID, PW
+  const setUser = useSetRecoilState<userType | null>(userState);
+
+  const onIDChange = (
+    idEvent: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setUserID(idEvent.target.value);
+  };
+
+  const onPWChange = (
+    pwEvent: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setUserPW(pwEvent.target.value);
+  };
+
+  const onLoginBtnClick = () => {
+    setUser({ userID, userPW });
+    router.push("/");
+  };
+
   return (
     <StyledLoginPageContainer>
       <LoginTitle />
@@ -27,6 +59,9 @@ function LoginPageContainer() {
         variant="outlined"
         fullWidth
         margin="normal"
+        required
+        value={userID}
+        onChange={onIDChange}
       />
       <TextField
         id="passwordForm"
@@ -35,12 +70,16 @@ function LoginPageContainer() {
         fullWidth
         margin="normal"
         type="password"
+        required
+        value={userPW}
+        onChange={onPWChange}
       />
       <Button
         variant="contained"
         fullWidth
         size="large"
         sx={{ height: 56, margin: "20px 0" }}
+        onClick={onLoginBtnClick}
       >
         로그인
       </Button>
