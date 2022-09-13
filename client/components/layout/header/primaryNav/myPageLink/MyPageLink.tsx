6eeb26fcbc414c7dd7dd61faa6773b8f5bca8542
nacean <nacean@naver.com>
2022-09-13 22:43:06 +0900

    :art: 로그인 유무에 따라 로그인 / 마이페이지 link 바뀌도록 변경

--- client/components/layout/header/primaryNav/myPageLink/MyPageLink.tsx
+++ client/components/layout/header/primaryNav/myPageLink/MyPageLink.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import styled from "styled-components";
+import userState from "atoms/loginAtoms/userState";
+import { useRecoilValue } from "recoil";
+import userType from "types/userTypes/userType";
 
 const MyPageAnchor = styled.a`
-  font-size: 16px;
+  font-size: 20px;
   display: flex;
   align-items: center;
   color: #6f767d;
@@ -20,15 +23,22 @@ const StyledMyPageLogo = styled.div`
 
 // Navigation 에서 myPage로 가는 버튼
 function MyPageLink() {
+  const user = useRecoilValue<userType | null>(userState);
   return (
-    <Link className="myPageLink" href="/" passHref>
+    <Link
+      className="myPageLink"
+      href={user ? "/myPage" : "/loginPage"}
+      passHref
+    >
       <MyPageAnchor>
-        <MyPageWord>마이페이지</MyPageWord>
-        <StyledMyPageLogo>
-          <PersonOutlineOutlinedIcon
-            sx={{ fontSize: "30px", color: "#6F767D" }}
-          />
-        </StyledMyPageLogo>
+        <MyPageWord>{user ? "마이페이지" : "로그인"}</MyPageWord>
+        {user && (
+          <StyledMyPageLogo>
+            <PersonOutlineOutlinedIcon
+              sx={{ fontSize: "30px", color: "#6F767D" }}
+            />
+          </StyledMyPageLogo>
+        )}
       </MyPageAnchor>
     </Link>
   );
