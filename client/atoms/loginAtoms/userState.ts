bbf5d846d3e9160f4b5d98d9ec358bf3b1d08dd8
nacean <nacean@naver.com>
2022-09-13 22:39:28 +0900

    :hammer: user atom type null 값 허용하게끔 변경

--- client/atoms/loginAtoms/userState.ts
+++ client/atoms/loginAtoms/userState.ts
@@ -1,7 +1,7 @@
 import { atom } from "recoil";
 import userType from "types/userTypes/userType";
 
-const userState = atom<userType>({
+const userState = atom<userType | null>({
   key: "userState",
   default: null,
 });
