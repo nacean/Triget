a478db905f0f4292bc4822df026ad3e8ea5856fd
nacean <nacean@naver.com>
2022-09-25 17:14:13 +0900

    :lipstick: triget 메인 로고 style 변경

--- client/components/layout/header/primaryNav/title/Title.tsx
+++ client/components/layout/header/primaryNav/title/Title.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import styled from "styled-components";
+import { theme } from "styles/theme";
 
 const TitleAnchor = styled.a`
   font-size: 40px;
   transition: 0.3s all;
-  &:hover {
-    color: #98dee3;
-  }
+  color: ${theme.colors.mainblue};
+  font-style: italic;
+  font-weight: 800;
 `;
 
 // navigation 에서 title로 가는 버튼
