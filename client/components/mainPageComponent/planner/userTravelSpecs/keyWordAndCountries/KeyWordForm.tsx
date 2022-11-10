24d57c50fefdf039068b7b90bd9cd061c70c6223
nacean <nacean@naver.com>
2022-11-10 20:55:09 +0900

    :sparkles: keywordForm 에서 placeholder 나오게끔 구현

--- client/components/mainPageComponent/planner/userTravelSpecs/keyWordAndCountries/KeyWordForm.tsx
+++ client/components/mainPageComponent/planner/userTravelSpecs/keyWordAndCountries/KeyWordForm.tsx
@@ -1,11 +1,4 @@
-import {
-  FormControl,
-  InputAdornment,
-  InputLabel,
-  MenuItem,
-  Select,
-  styled,
-} from "@mui/material";
+import { InputAdornment, MenuItem } from "@mui/material";
 import ChatIcon from "@mui/icons-material/Chat";
 import { useRecoilState } from "recoil";
 import travelKeywordState from "atoms/plannerAtoms/travelKeywordState";
@@ -43,23 +36,6 @@ const keywords = [
   },
 ];
 
-const StyledSelect = styled(Select)`
-  & .MuiOutlinedInput-notchedOutline {
-    border-color: #ebebeb;
-    background-color: #fafafa;
-    border-radius: 8px;
-  }
-  & .MuiInputBase-input {
-    z-index: 1300;
-  }
-  & .MuiFormLabel-root {
-    font-size: 16px;
-    font-weight: 500;
-    letter-spacing: -0.45px;
-    color: #abadae;
-  }
-`;
-
 function KeyWordForm() {
   const [travelKeywordValue, setTravelKeywordValue] =
     useRecoilState<string>(travelKeywordState);
@@ -72,63 +48,46 @@ function KeyWordForm() {
 
   const getKeywords = () =>
     keywords.map((keyword: { value: string; label: string }) => (
-      <MenuItem value={keyword.value}>{keyword.label}</MenuItem>
+      <MenuItem key={keyword.value} value={keyword.value}>
+        {keyword.label}
+      </MenuItem>
     ));
 
   return (
-    // <StyledTextField
-    //   id="select-keyword"
-    //   select
-    //   label="Travel Keyword"
-    //   placeholder="가고 싶은 여행의 키워드를 입력해 주세요"
-    //   InputProps={{
-    //     startAdornment: (
-    //       <InputAdornment position="start">
-    //         <ChatIcon
-    //           fontSize="small"
-    //           sx={{
-    //             color: travelKeywordValue ? theme.colors.mainblue : "#aaa",
-    //             zIndex: 1200,
-    //           }}
-    //         />
-    //       </InputAdornment>
-    //     ),
-    //   }}
-    //   fullWidth
-    //   value={travelKeywordValue}
-    //   onChange={onTravelKeywordChange}
-    //   sx={{ marginBottom: "16px" }}
-    // >
-    //   {keywords.map(keywordParam => (
-    //     <MenuItem key={keywordParam.value} value={keywordParam.value}>
-    //       {keywordParam.label}
-    //     </MenuItem>
-    //   ))}
-    // </StyledTextField>
-    <StyledSelect
+    <StyledTextField
       id="select-keyword"
-      labelId="Travel Keyword"
-      label="keyword"
+      label="Travel Keyword"
+      select
       fullWidth
+      placeholder="yeeees"
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <ChatIcon
+              fontSize="small"
+              sx={{
+                color: travelKeywordValue ? theme.colors.mainblue : "#aaa",
+                zIndex: 1200,
+              }}
+            />
+          </InputAdornment>
+        ),
+        sx: {
+          color: travelKeywordValue !== "" ? "#000" : "#aaa",
+        },
+      }}
+      SelectProps={{
+        defaultValue: "ww",
+        displayEmpty: true,
+        renderValue:
+          travelKeywordValue !== "" ? undefined : () => "키워드를 선택해주세요",
+      }}
       value={travelKeywordValue}
       onChange={onTravelKeywordChange}
       sx={{ marginBottom: "16px" }}
-      placeholder="wow"
-      renderValue={selected => (
-        <ChatIcon
-          fontSize="small"
-          sx={{
-            zIndex: 1200,
-            marginLeft: "4px",
-          }}
-        />
-      )}
     >
-      <MenuItem disabled value="">
-        <em>가고 싶은 여행의 키워드를 입력해 주세요</em>
-      </MenuItem>
       {getKeywords()}
-    </StyledSelect>
+    </StyledTextField>
   );
 }
 
